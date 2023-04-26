0c26eba6f38b7b886edcdb3b3f6a1bd00c166215
Angel Borja <borjaangel244@gmail.com>
2023-04-26 18:28:50 -0400

    update

---
+++ src/main/java/io/zipcoder/interfaces/Students.java
@@ -0,0 +1,17 @@
+package io.zipcoder.interfaces;
+
+public final class Students {
+
+    private static final Students INSTANCE = new Students();
+
+
+
+    public Students getInstance(){
+        return INSTANCE;
+    }
+
+
+
+
+
+}
