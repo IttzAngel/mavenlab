e18f83be90bbea4795652cf70558f1de342b0ef0
Angel Borja <borjaangel244@gmail.com>
2023-05-13 10:16:41 -0400

    update

--- src/main/java/io/zipcoder/interfaces/Instructors.java
+++ src/main/java/io/zipcoder/interfaces/Instructors.java
@@ -6,12 +6,12 @@ public final class Instructors extends People{
 
 
     private Instructors(){
-        Instructor instructor1 = new Instructor(7936, "Mikaila");
-        Instructor instructor2 = new Instructor(3871, "Peter");
-        Instructor instructor3 = new Instructor(4209, "Bobby");
-        INSTANCE.add(instructor1);
-        INSTANCE.add(instructor2);
-        INSTANCE.add(instructor3);
+//        Instructor instructor1 = new Instructor(7936, "Mikaila");
+//        Instructor instructor2 = new Instructor(3871, "Peter");
+//        Instructor instructor3 = new Instructor(4209, "Bobby");
+//        INSTANCE.add(instructor1);
+//        INSTANCE.add(instructor2);
+//        INSTANCE.add(instructor3);
     }
 
     public static Instructors getInstance(){
--- src/test/java/io/zipcoder/interfaces/TestInstructors.java
+++ src/test/java/io/zipcoder/interfaces/TestInstructors.java
@@ -11,34 +11,34 @@ public class TestInstructors {
     @Test
     public void test(){
 
-        Instructors instructors = Instructors.getInstance();
-        List<Instructor> expectedInstructors = new ArrayList<>();
-        expectedInstructors.add(new Instructor(7936, "Mikaila"));
-        expectedInstructors.add(new Instructor(3871, "Peter"));
-        expectedInstructors.add(new Instructor(4209, "Bobby"));
+//        Instructors instructors = Instructors.getInstance();
+//        List<Instructor> expectedInstructors = new ArrayList<>();
+//        expectedInstructors.add(new Instructor(7936, "Mikaila"));
+//        expectedInstructors.add(new Instructor(3871, "Peter"));
+//        expectedInstructors.add(new Instructor(4209, "Bobby"));
 
 //        for (Instructor expectedInstructor : expectedInstructors) {
 //            Assert.assertTrue(instructors.contains(expectedInstructor));
 //        }
 
-        for (Instructor expectedInstructor : expectedInstructors) {
-            boolean containsExpected = instructors.contains(expectedInstructor);
-            System.out.println("Expected: " + expectedInstructor + ", Actual: " + (containsExpected ? expectedInstructor : "NOT FOUND"));
-            Assert.assertTrue(containsExpected);
-        }
+//        for (Instructor expectedInstructor : expectedInstructors) {
+//            boolean containsExpected = instructors.contains(expectedInstructor);
+//            System.out.println("Expected: " + expectedInstructor + ", Actual: " + (containsExpected ? expectedInstructor : "NOT FOUND"));
+//            Assert.assertTrue(containsExpected);
+//        }
 
-//        List<Instructor> zipCode = new ArrayList<>();
-//
+        List<Instructor> zipCode = new ArrayList<>();
+
 //        zipCode.add(new Instructor(7936, "Mikaila"));
 //        zipCode.add(new Instructor(3871, "Peter"));
 //        zipCode.add(new Instructor(4209, "Bobby"));
 
-//        Instructors instructors = Instructors.getInstance();
-//
-//        Assert.assertTrue(instructors.contains(new Instructor(7936, "Mikaila")));
+        Instructors instructors = Instructors.getInstance();
 
-//        for (Instructor instructor : zipCode) {
-//            Assert.assertTrue(instructors.contains(instructor));
-//        }
+        //Assert.assertTrue(instructors.contains(new Instructor(7936, "Mikaila")));
+
+        for (Instructor instructor : zipCode) {
+            Assert.assertTrue(instructors.contains(instructor));
+        }
     }
 }
